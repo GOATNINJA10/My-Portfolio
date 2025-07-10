6c3d13aaae67986859afa83bd2a39372e2dd0663
GOATNINJA10 <k.bramhecha@somaiya.edu>
2025-07-10 22:50:48 +0530

    Made minor UI Changes

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -9,19 +9,7 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 gsap.registerPlugin(ScrollTrigger);
 
 const TechStack = () => {
-  useGSAP(() =>{
-    gsap.fromTo('.tech-card', {v: 50, opacity: 0},{
-        y: 0,
-        opacity: 1,
-        duration: 1,
-        ease: 'power2.inOut',
-        stagger: 0.2,
-        scrollTrigger : {
-            trigger: "#skills",
-            start: 'top center'
-        }
-    })
-  })  
+   
   
   return (
     <div id='skills' className='flex-center section-padding'>
@@ -33,7 +21,7 @@ const TechStack = () => {
             <div className='tech-grid'>
                 {techStackIcons.map((icon) =>(
                     <div key={icon.name} className='card-border tech-card overflow-hidden group xl:rounded-full rounded-lg'>
-                        <div className='tech-card-animated-bg' />
+                            {/* <div className='tech-card-animated-bg' /> */}
                         <div className='tech-card-content'>
                             <div className='tech-icon-wrapper'>
                                 <TechIcon model={icon} />
